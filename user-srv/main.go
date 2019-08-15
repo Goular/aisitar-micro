a371855859939b5744ff2f8a80ffce2f122a3b43
goular <zhaojt_cs@126.com>
2019-08-15 09:12:58 +0800

    提交代码

--- user-srv/main.go
+++ user-srv/main.go
@@ -1,35 +1,55 @@
 package main
 
 import (
-	"github.com/micro/go-micro/util/log"
+	"aisitar-micro/user-srv/basic"
+	"aisitar-micro/user-srv/basic/config"
+	"aisitar-micro/user-srv/handler"
+	"aisitar-micro/user-srv/model"
+	s "aisitar-micro/user-srv/proto/user"
+	"fmt"
+	"github.com/micro/cli"
 	"github.com/micro/go-micro"
-	"./aisitar-micro/user-srv/handler"
-	"./aisitar-micro/user-srv/subscriber"
-
-	user "./aisitar-micro/user-srv/proto/user"
+	"github.com/micro/go-micro/registry"
+	"github.com/micro/go-micro/registry/consul"
+	"github.com/micro/go-micro/util/log"
+	"time"
 )
 
 func main() {
-	// New Service
+	// 初始化配置、数据库等信息
+	basic.Init()
+
+	// 使用consul注册
+	micReg := consul.NewRegistry(registryOptions)
+
+	// 新建服务
 	service := micro.NewService(
 		micro.Name("mu.micro.book.srv.user"),
+		micro.Registry(micReg),
 		micro.Version("latest"),
 	)
 
-	// Initialise service
-	service.Init()
+	// 服务初始化
+	service.Init(
+		micro.Action(func(c *cli.Context) {
+			// 初始化模型层
+			model.Init()
+			// 初始化handler
+			handler.Init()
+		}),
+	)
 
-	// Register Handler
-	user.RegisterUserHandler(service.Server(), new(handler.User))
+	// 注册服务
+	s.RegisterUserHandler(service.Server(), new(handler.Service))
 
-	// Register Struct as Subscriber
-	micro.RegisterSubscriber("mu.micro.book.srv.user", service.Server(), new(subscriber.User))
-
-	// Register Function as Subscriber
-	micro.RegisterSubscriber("mu.micro.book.srv.user", service.Server(), subscriber.Handler)
-
-	// Run service
+	// 启动服务
 	if err := service.Run(); err != nil {
 		log.Fatal(err)
 	}
+}
+
+func registryOptions(ops *registry.Options) {
+	consulCfg := config.GetConsulConfig()
+	ops.Timeout = time.Second * 5
+	ops.Addrs = []string{fmt.Sprintf("%s:%d", consulCfg.GetHost(), consulCfg.GetPort())}
 }
---
+++ user-srv/model/model.go
@@ -0,0 +1,8 @@
+package model
+
+import "aisitar-micro/user-srv/model/user"
+
+// Init 初始化模型层
+func Init() {
+	user.Init()
+}
