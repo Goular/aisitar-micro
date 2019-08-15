55ab8c6d25d9a679d2a3ed5b2aff5c13f72c7939
goular <zhaojt_cs@126.com>
2019-08-15 18:31:47 +0800

    part3提交代码

---
+++ orders-web/main.go
@@ -0,0 +1,59 @@
+package main
+
+import (
+	"aisitar-micro/basic"
+	"aisitar-micro/basic/config"
+	"aisitar-micro/orders-web/handler"
+	"fmt"
+	"net/http"
+	"time"
+
+	"github.com/micro/cli"
+	"github.com/micro/go-micro/registry"
+	"github.com/micro/go-micro/registry/consul"
+	"github.com/micro/go-micro/util/log"
+	"github.com/micro/go-micro/web"
+)
+
+func main() {
+	// 初始化配置
+	basic.Init()
+
+	// 使用consul注册
+	micReg := consul.NewRegistry(registryOptions)
+
+	// 创建新服务
+	service := web.NewService(
+		web.Name("mu.micro.book.web.orders"),
+		web.Version("latest"),
+		web.Registry(micReg),
+		web.Address(":8091"),
+	)
+
+	// 初始化服务
+	if err := service.Init(
+		web.Action(
+			func(c *cli.Context) {
+				// 初始化handler
+				handler.Init()
+			}),
+	); err != nil {
+		log.Fatal(err)
+	}
+
+	// 新建订单接口
+	authHandler := http.HandlerFunc(handler.New)
+	service.Handle("/orders/new", handler.AuthWrapper(authHandler))
+	service.HandleFunc("/", handler.Hello)
+
+	// 运行服务
+	if err := service.Run(); err != nil {
+		log.Fatal(err)
+	}
+}
+
+func registryOptions(ops *registry.Options) {
+	consulCfg := config.GetConsulConfig()
+	ops.Timeout = time.Second * 5
+	ops.Addrs = []string{fmt.Sprintf("%s:%d", consulCfg.GetHost(), consulCfg.GetPort())}
+}
